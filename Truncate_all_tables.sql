4015ae627431be4b38179acf38a33a6c2cd1446b
godknowsdevil17@gmail.com <godknowsdevil17@gmail.com>
2026-01-17 11:04:42 +0000

    dev

---
+++ Truncate_all_tables.sql
@@ -0,0 +1,13 @@
+-- Databricks notebook source
+Truncate table mini_project.bronze.customers;
+Truncate table mini_project.bronze.order_items;
+Truncate table mini_project.bronze.orders;
+Truncate table mini_project.bronze.products;
+Truncate table mini_project.gold.category_sales;
+Truncate table mini_project.gold.customer_lifetime_value;
+Truncate table mini_project.gold.daily_sales;
+Truncate table mini_project.gold.top_products;
+Truncate table mini_project.silver.customers;
+Truncate table mini_project.silver.order_items;
+Truncate table mini_project.silver.orders;
+Truncate table mini_project.silver.products;
